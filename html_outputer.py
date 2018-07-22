efa6f9fdd97461e0186fd072e4467d42ff4da1f3
Mona <jinzh469@student.liu.se>
2018-07-21 17:13:53 -0700

    Update html_outputer.py

--- html_outputer.py
+++ html_outputer.py
@@ -16,7 +16,7 @@ class HtmlOutputer(object):
 
 		for data in self.datas:
 			fout.write("<tr>")
-			fout.write("<td>%s</td>" %data['url'])
+			fout.write("<td>%s</td>" %data['url'].encode('utf-8'))
 			fout.write("<td>%s</td>" %data['title'].encode('utf-8'))
 			fout.write("<td>%s</td>" %data['summary'].encode('utf-8'))
 			fout.write("</tr>")
